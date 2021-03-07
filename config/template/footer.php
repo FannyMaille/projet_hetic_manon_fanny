bba74ede5d511d50b940dea9dbefdf9468e0204b
Fanny MAILLE <75837977+FannyMaille@users.noreply.github.com>
2021-03-07 14:24:21 +0100

    [footer]

--- config/template/footer.php
+++ config/template/footer.php
@@ -1,5 +1,22 @@
-<footer>je suis le footer</footer>
-</div>
+<footer class="footer">
+  <p>Pages Produits</p>
+  <ul>
+    <li>Accueil</li>
+    <li>Article 1</li>
+    <li>Article 2</li>
+    <li>Article 3</li>
+    <li>Pannier</li>
+  </ul>
+  <p>Pages interractives</p>
+  <ul>
+    <li>Connexion</li>
+    <li>Login</li>
+    <li>Inscription</li>
+    <li>Article 3</li>
+  </ul>
+  <p>© Canon 2021 - Tous droits réservés</p>
+</footer>
+
 
 <script src="asset/script/script.js"></script>
 </body>
