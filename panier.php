353cd61ac66fd6f1e4bdb5facfb648dd30f48092
Manon <manon.bonnet@hetic.net>
2021-03-08 19:58:57 +0100

    [html/css] page panier html integration

--- panier.php
+++ panier.php
@@ -2,6 +2,46 @@
 <header>
     <?php include 'config/template/nav.php'; ?>
 </header>
-<h2 class="text-center mt-5 mb-5">Page panier</h2>
-<hr>
+<section>
+    <h1 class="text-center mt-5 mb-5">Page panier</h1>
+
+    <ul class="panier-liste">
+        <li class="panier-produit p-3">
+            <div class="ml-3">
+                <h2>[Nom du produit]</h2>
+                <p>[Prix]</p>
+                <form class="panier-quantite" action="" method="post">
+                    <input type="submit" name="plus" value="+"></button>
+                    <p class="pr-2 pl-2">[quantite]</p>
+                    <input type="submit" name="minus" value="-"></button>
+                </form>
+            </div>
+            <img src="asset/img/herofullimage.jpg">
+        </li>
+        <li class="panier-produit p-3">
+            <div class="ml-3">
+                <h2>[Nom du produit]</h2>
+                <p>[Prix]</p>
+                <form class="panier-quantite" action="" method="post">
+                    <input type="submit" name="plus" value="+"></button>
+                    <p class="pr-2 pl-2">[quantite]</p>
+                    <input type="submit" name="minus" value="-"></button>
+                </form>
+            </div>
+            <img src="asset/img/herofullimage.jpg">
+        </li>
+    </ul>
+
+    <div class="panier-total text-center mb-5">
+        <div class="d-flex justify-content-between mt-3">
+            <p>Prix total :</p>
+            <p>[Prix total]€</p>
+        </div>
+        <form>
+            <input class="w-100" type="submit" name="acheter" value="Passer à l'achat">
+        </form>
+    </div>
+    
+</section>
+
 <?php include 'config/template/footer.php'; ?>
