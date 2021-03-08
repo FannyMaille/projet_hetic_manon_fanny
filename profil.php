9fe6d0f3810b302d427fa72117dab7828cce6af0
Manon <manon.bonnet@hetic.net>
2021-03-08 19:10:44 +0100

    [html/css] profil page html integration

--- profil.php
+++ profil.php
@@ -2,6 +2,84 @@
 <header>
     <?php include 'config/template/nav.php'; ?>
 </header>
-<h2 class="text-center mt-5 mb-5">Page profil</h2>
-<hr>
+<section class="sectformulaire">
+    <h1 class="text-center mt-5">Bonjour [pseudo] !</h1>
+    <p class="text-center mb-5">Vous êtes [simple membre / admin]</p>
+
+    <ul class="formulaireprofil">
+        <li>
+            <form action="" method="post">
+                <div>
+                    <label for="pseudo">Pseudo :</label>
+                    <input type="hidden" name="pseudo" id="pseudo">
+                    <p>[pseudo]</p>
+                </div>
+                <div>
+                    <input type="submit" value="Modifier" name="modifier-pseudo">
+                </div>
+            </form>
+        </li>
+        <li>
+            <form action="" method="post">
+                <div>
+                    <label for="mdp">Mot de passe :</label>
+                    <input type="hidden" name="new-mdp" id="new-mdp">
+                    <p>*******</p>
+                </div>
+                <div>
+                    <input type="submit" value="Modifier" name="modifier-mdp">
+                </div>
+            </form>
+        </li>
+        <li>
+            <form action="" method="post">
+                <div>
+                    <label for="civilite">Civilité :</label>
+                    <input type="hidden" name="civilite" id="civilite">
+                    <p>[sexe]</p>
+                </div>
+                <div>
+                    <input type="submit" value="Modifier" name="modifier-civilite">
+                </div>
+            </form>
+        </li>
+        <li>
+            <form action="" method="post">
+                <div>
+                    <label for="adresse">Adresse postale :</label>
+                    <input type="hidden" name="adresse" id="adresse">
+                    <p>[numrue] [nom de rue] [CP], [ville]</p>
+                </div>
+                <div>
+                    <input type="submit" value="Modifier" name="modifier-adresse">
+                </div>
+            </form>
+        </li>
+        <li>
+            <form action="" method="post">
+                <div>
+                    <label for="tel">Numéro de téléhpone :</label>
+                    <input type="hidden" name="tel" id="tel">
+                    <p>[numéro de téléphone]</p>
+                </div>
+                <div>
+                    <input type="submit" value="Modifier" name="modifier-tel">
+                </div>
+            </form>
+        </li>
+        <li>
+            <form action="" method="post">
+                <div>
+                    <label for="mail">Adresse mail :</label>
+                    <input type="hidden" name="mail" id="mail">
+                    <p>[adresse mail]</p>
+                </div>
+                <div>
+                    <input type="submit" value="Modifier" name="modifier-mail">
+                </div>
+            </form>
+        </li>
+    </ul>
+    
+</section>
 <?php include 'config/template/footer.php'; ?>
