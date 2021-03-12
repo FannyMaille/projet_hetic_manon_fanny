4e361ea30ff023c2fe77607f2c848965a61fcc91
Manon <manon.bonnet@hetic.net>
2021-03-12 14:57:58 +0100

    [php] connexion a la base de donnees

--- config/init.php
+++ config/init.php
@@ -1,6 +1,21 @@
 <?php
 
 //connexion PDO
+define('HOSTNAME', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', 'root');
+define('DATABASE', 'maillem599');
+
+$dsn = "mysql:host=" . HOSTNAME . ';dbname=' . DATABASE;
+
+try{
+  $pdo = new PDO($dsn, USERNAME, PASSWORD);
+} catch(PDOException $e){
+  die('<ul><li>Erreur sur le fichier : ' . 
+  $e -> getfile() . '</li><li>Erreur à la ligne : ' .
+  $e -> getLine() . 'Message d\'erreur : ' .
+  $e -> getMessage() . '</li></ul>');
+}
 
 //variable d'affichage etc. 
 
