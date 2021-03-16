84473e49dd0f209a3b5e9b9cc28087fecabae749
Manon <manon.bonnet@hetic.net>
2021-03-16 12:13:37 +0100

    [html/js] profil modifier buttons functionnal

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,1 +1,36 @@
 "use strict";
+
+// ------------------------ Profil.php
+// Get all the elements
+const modifyProfilButtons = document.getElementsByClassName('modifier-profil');
+let previousChange;
+
+// Add the listeners
+for(let i = 0; i < modifyProfilButtons.length; i++){
+  modifyProfilButtons[i].addEventListener("click", changeProfilElement);
+}
+
+// functions
+function changeProfilElement(){
+  let buttonValue = this.value;
+  let form = document.getElementsByClassName(buttonValue)[0];
+  let hiddenElements = form.getElementsByClassName('hidden');
+  let profilElement = form.getElementsByClassName('profil-element')[0];
+  profilElement.style.display = 'none';
+
+  for(let i = 0 ; i < hiddenElements.length ; i++){
+    hiddenElements[i].style.display = "block";
+  }
+
+  if(previousChange){
+    console.log('ok');
+    let previousHiddenElements = previousChange.getElementsByClassName('hidden');
+    let previousProfilElement = previousChange.getElementsByClassName('profil-element')[0];
+    previousProfilElement.style.display = 'block';
+    for(let i = 0 ; i < previousHiddenElements.length ; i++){
+      previousHiddenElements[i].style.display = "none";
+    }
+  }
+
+  previousChange = form;
+}
--- profil.php
+++ profil.php
@@ -8,83 +8,77 @@
 
     <ul class="formulaireprofil">
         <li>
-            <form action="init.php" method="post">
-                <div>
-                    <label for="pseudo">Pseudo :</label>
-                    <input type="text" class="hidden" name="pseudo" id="pseudo">
-                    <p>[pseudo]</p>
-                </div>
-                <div>
-                    <input type="submit" value="Modifier" name="modifier-pseudo">
-                </div>
+            <form action="init.php" method="post" class="profil-pseudo">
+                <label for="pseudo">Pseudo :</label>
+                <input type="text" class="hidden" name="pseudo" id="pseudo">
+                <input type="submit" class="hidden" name="modifier-pseudo" value="Valider">
+                <p class="profil-element">[pseudo]</p>
             </form>
+            <div>
+                <button type="submit" class="modifier-profil" value="profil-pseudo">Modifier</button>
+            </div>
         </li>
         <li>
-            <form action="init.php" method="post">
-                <div>
-                    <label for="new-mdp">Mot de passe :</label>
-                    <input type="password" class="hidden" name="new-mdp" id="new-mdp">
-                    <p>*******</p>
-                </div>
-                <div>
-                    <input type="submit" value="Modifier" name="modifier-mdp">
-                </div>
+            <form action="init.php" method="post" class="profil-mdp">
+                <label for="new-mdp">Mot de passe :</label>
+                <input type="password" class="hidden" name="new-mdp" id="new-mdp">
+                <input type="submit" class="hidden" name="modifier-mdp" value="Valider">
+                <p class="profil-element">*******</p>
             </form>
+            <div>
+                <button type="submit" class="modifier-profil" value="profil-mdp">Modifier</button>
+            </div>
         </li>
         <li>
-            <form action="init.php" method="post">
-                <div>
-                    <label for="civilite">Civilité :</label>
-                    <input type="text" class="hidden" name="civilite" id="civilite">
-                    <p>[sexe]</p>
-                </div>
-                <div>
-                    <input type="submit" value="Modifier" name="modifier-civilite">
-                </div>
+            <form action="init.php" method="post" class="profil-civil">
+                <label for="civilite">Civilité :</label>
+                <input type="text" class="hidden" name="civilite" id="civilite">
+                <input type="submit" class="hidden" name="modifier-civil" value="Valider">
+                <p class="profil-element">[sexe]</p>
             </form>
+            <div>
+                <button type="submit" class="modifier-profil" value="profil-civil">Modifier</button>    
+            </div>
         </li>
         <li>
-            <form action="init.php" method="post">
-                <div>
-                    <p>Adresse postale :</p>
-                    <label for="numrue" class="hidden">Numéro de rue :</label>
-                    <input type="number" class="hidden" name="numrue" id="numrue">
-                    <label for="rue" class="hidden">Nom de rue :</label>
-                    <input type="text" class="hidden" name="rue" id="rue">
-                    <label for="cp" class="hidden">Code postal :</label>
-                    <input type="number" class="hidden" name="cp" id="cp">
-                    <label for="ville" class="hidden">Ville :</label>
-                    <input type="text" class="hidden" name="ville" id="ville">
-                    <p>[numrue] [nom de rue] [CP], [ville]</p>
-                </div>
-                <div>
-                    <input type="submit" value="Modifier" name="modifier-adresse">
-                </div>
+            <form action="init.php" method="post" class="profil-adresse">
+                <p>Adresse postale :</p>
+                <label for="numrue" class="hidden">Numéro de rue :</label>
+                <input type="number" class="hidden" name="numrue" id="numrue">
+                <label for="rue" class="hidden">Nom de rue :</label>
+                <input type="text" class="hidden" name="rue" id="rue">
+                <label for="cp" class="hidden">Code postal :</label>
+                <input type="number" class="hidden" name="cp" id="cp">
+                <label for="ville" class="hidden">Ville :</label>
+                <input type="text" class="hidden" name="ville" id="ville">
+                <input type="submit" class="hidden" name="modifier-adresse" value="Valider">
+                <p class="profil-element">[numrue] [nom de rue] [CP], [ville]</p>
             </form>
+            <div>
+                <button type="submit" class="modifier-profil" value="profil-adresse">Modifier</button>    
+            </div>
         </li>
         <li>
-            <form action="init.php" method="post">
-                <div>
-                    <label for="tel">Numéro de téléhpone :</label>
-                    <input type="tel" class="hidden" name="tel" id="tel">
-                    <p>[numéro de téléphone]</p>
-                </div>
-                <div>
-                    <input type="submit" value="Modifier" name="modifier-tel">
-                </div>
+            <form action="init.php" method="post" class="profil-tel">
+                <label for="tel">Numéro de téléhpone :</label>
+                <input type="tel" class="hidden" name="tel" id="tel">
+                <input type="submit" class="hidden" name="modifier-tel" value="Valider">
+                <p class="profil-element">[numéro de téléphone]</p>
             </form>
+            <div>
+                <button type="submit" class="modifier-profil" value="profil-tel">Modifier</button>
+            </div>
         </li>
         <li>
-            <form action="init.php" method="post">
-                <div>
-                    <label for="mail">Adresse mail :</label>
-                    <input type="email" class="hidden" name="mail" id="mail">
-                    <p>[adresse mail]</p>
-                </div>
-                <div>
-                    <input type="submit" value="Modifier" name="modifier-mail">
-                </div>
+            <form action="init.php" method="post" class="profil-mail">
+                <label for="mail">Adresse mail :</label>
+                <input type="email" class="hidden" name="mail" id="mail">
+                <input type="submit" class="hidden" name="modifier-mail" value="Valider">
+                <p class="profil-element">[adresse mail]</p>
             </form>
+            <div>
+                <button type="submit" class="modifier-profil" value="profil-mail">Modifier</button>
+            </div>
         </li>
     </ul>
     
