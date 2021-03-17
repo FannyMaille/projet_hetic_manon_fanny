e92fe4a8e0b2bbc819b9a5db2b825b7a830382d3
Manon <manon.bonnet@hetic.net>
2021-03-17 12:10:39 +0100

    [php] set problem of session suppressed in main menu

--- config/template/nav.php
+++ config/template/nav.php
@@ -1,13 +1,13 @@
 <nav class="nav">
-    <a class="nav-link" href="index.php">Accueil</a>
+    <a class="nav-link" href="index.php?access=<?php if(isset($_GET['access'])){ echo $_GET['access']; } ?>">Accueil</a>
     <?php if(!isset($_SESSION['user'])){ ?>
-    <a class="nav-link" href="inscription.php">Inscription</a>
+    <a class="nav-link" href="inscription.php?access=<?php if(isset($_GET['access'])){ echo $_GET['access']; } ?>">Inscription</a>
     <?php }
     else if (isset($_SESSION['user']) && $_GET['access'] == 'forbidden'){ ?>
-    <a class="nav-link" href="login.php">Login</a>
+    <a class="nav-link" href="login.php?access=<?= $_GET['access'] ?>">Login</a>
     <?php }
     else {?>
-    <a class="nav-link" href="profil.php">Profil</a>
+    <a class="nav-link" href="profil.php?access=<?php if(isset($_GET['access'])){ echo $_GET['access']; } ?>">Profil</a>
     <?php } ?>
-    <a class="nav-link" href="panier.php">Panier</a>
+    <a class="nav-link" href="panier.php?access=<?php if(isset($_GET['access'])){ echo $_GET['access']; } ?>">Panier</a>
 </nav>
