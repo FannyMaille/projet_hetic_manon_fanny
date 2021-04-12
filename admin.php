443320233a7ce8129f1fd6bf864c944fd692457e
Manon <manon.bonnet@hetic.net>
2021-04-12 12:22:05 +0200

    feuilles des accès pour le prof

--- admin.php
+++ admin.php
@@ -90,9 +90,6 @@ if(isset($_POST['add-new-produit'])){
       </form>
     </section>
 
-    
-    <a href="index.php?session=destroy" class="btnclassique">Déconnexion</a>
-
     <!-- //https://www.w3schools.com/howto/howto_js_filter_lists.asp 
     if willing to make a search bar if multiple products -->
 
